2bc749a340448d994c1ab223021c2a65ebebe043
Ojelade Olamide Emmanuel <emmanuelojelade7@gmail.com>
2025-11-19 22:06:57 +0100

    update

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -20,7 +20,7 @@ export class MailerService {
 
     this.transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
-      port: 465,
+      port: 587,
       secure: false,
       auth: {
         user: 'emmanuelojelade7@gmail.com',
